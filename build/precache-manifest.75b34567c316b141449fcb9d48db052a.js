bd60c44c9b20b4f9a733be8ff9b2a607ac56566e
Muqker <muqker@muqker.com>
2020-08-09 13:05:29 +0300

    unignore build

---
+++ build/precache-manifest.75b34567c316b141449fcb9d48db052a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee8493cd70199922657595eabcf4662",
+    "url": "/index.html"
+  },
+  {
+    "revision": "353a1e9e428f88a5f6d3",
+    "url": "/static/css/main.14546e1a.chunk.css"
+  },
+  {
+    "revision": "db977e8610798d8a546c",
+    "url": "/static/js/2.a64c161a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a64c161a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "353a1e9e428f88a5f6d3",
+    "url": "/static/js/main.d66f64b8.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
